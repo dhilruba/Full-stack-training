abcff38c6a09656b68eb33c408d0a045e184bc4a
dhilruba <dhilruba.v@appinessworld.com>
2022-05-04 17:19:24 +0530

    1 st commit

---
+++ 4may22/app.js
@@ -0,0 +1,5 @@
+console.log("hiiii");
+if (5>2) {
+    console.log("its greater");
+    
+}
---
+++ 4may22/if.js
@@ -0,0 +1,28 @@
+// function isEven(num){
+// if (num%2===0){
+//   console.log("The entered number is an even");
+// }
+// }
+
+// ......................................
+// const userInput=prompt("enter something");
+// if (userInput) {
+//     console.log("truthy")
+    
+// }
+// else{
+//     console.log("falsy")
+// }
+
+// ..............................
+// const age=10;
+// if (age<5 || age>8){
+//     console.log("correct age")
+// }
+// .................................
+// let lottoNumbers=[4,5,6,8,7,9];
+// console.log(lottoNumbers);
+// ...................................
+
+
+    
